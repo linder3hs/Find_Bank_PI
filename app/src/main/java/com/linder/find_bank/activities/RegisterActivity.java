b0815fb91ff7685aba3c9057f6752f02faa01747
Linder Hassinger <linderhassinger00@gmail.com>
2017-10-22 10:47:28 -0500

    Resideño del registro y UI de agente

--- app/src/main/java/com/linder/find_bank/activities/RegisterActivity.java
+++ app/src/main/java/com/linder/find_bank/activities/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,6 +38,7 @@ public class RegisterActivity extends AppCompatActivity {
         mail = (EditText) findViewById(R.id.emailRegister);
         contra = (EditText) findViewById(R.id.passwordRegister);
         contaAgian = (EditText) findViewById(R.id.passwordRegisteragain);
+
 
         finaRegister = (Button) findViewById(R.id.finalRegister);
         finaRegister.setOnClickListener(new View.OnClickListener() {
